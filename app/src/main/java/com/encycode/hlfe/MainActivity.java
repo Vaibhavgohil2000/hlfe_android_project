3203c7d8414239cff7e0e113f2729cdba12f3398
Shubham Vyas <svyas878@gmail.com>
2020-07-29 21:20:18 +0530

    splash screen activity intialized

--- app/src/main/java/com/encycode/hlfe/MainActivity.java
+++ app/src/main/java/com/encycode/hlfe/MainActivity.java
@@ -2,13 +2,55 @@ package com.encycode.hlfe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button splash,login,contact_us,about_us;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splash = (Button)findViewById(R.id.btn_splash_screen);
+        login = (Button)findViewById(R.id.btn_login);
+        contact_us = (Button)findViewById(R.id.btn_contact_us);
+        about_us = (Button)findViewById(R.id.btn_about_us);
+
+
+        splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SplashActivity.class);
+                startActivity(i);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SplashActivity.class);
+                startActivity(i);
+            }
+        });
+
+        contact_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SplashActivity.class);
+                startActivity(i);
+            }
+        });
+
+        about_us.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), SplashActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
